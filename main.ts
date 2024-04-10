d4f3f362767f55f1d9ae35c2eb00afde0ada884b
Gnlow <gnlow@protonmail.com>
2024-04-10 15:12:55 +0900

    chap4

--- main.ts
+++ main.ts
@@ -24,16 +24,84 @@ ctx.configure({
 
 const encoder = device.createCommandEncoder()
 
+const vertices = new Float32Array([
+    -0.8, -0.8,
+     0.8, -0.8,
+     0.8,  0.8,
+    
+    -0.8, -0.8,
+     0.8,  0.8,
+    -0.8,  0.8,
+])
+
+const vertexBuffer = device.createBuffer({
+    label: "Cell vertices",
+    size: vertices.byteLength,
+    usage: 0
+        | GPUBufferUsage.VERTEX
+        | GPUBufferUsage.COPY_DST,
+})
+
+device.queue.writeBuffer(
+    vertexBuffer,
+    0,
+    vertices,
+)
+
+const vertexBufferLayout: GPUVertexBufferLayout = {
+    arrayStride: Float32Array.BYTES_PER_ELEMENT * 2, // 4 * 2 = 8
+    attributes: [{
+        format: "float32x2",
+        offset: 0,
+        shaderLocation: 0,
+    }]
+}
+
+const cellShaderModule = device.createShaderModule({
+    label: "Cell shader",
+    code: `
+        @vertex
+        fn vertexMain(@location(0) pos: vec2f) ->
+        @builtin(position)
+        vec4f {
+            return vec4f(pos, 0, 1);
+        }
+
+        @fragment
+        fn fragmentMain() -> @location(0) vec4f {
+            return vec4f(1, 0, 0, 1);
+        }
+    `
+})
+
+const cellPipeline = device.createRenderPipeline({
+    label: "Cell pipeline",
+    layout: "auto",
+    vertex: {
+        module: cellShaderModule,
+        entryPoint: "vertexMain",
+        buffers: [vertexBufferLayout],
+    },
+    fragment: {
+        module: cellShaderModule,
+        entryPoint: "fragmentMain",
+        targets: [{ format }],
+    }
+})
+
 const pass = encoder.beginRenderPass({
     colorAttachments: [{
         view: ctx.getCurrentTexture().createView(),
         loadOp: "clear",
+        clearValue: [0, 0, 0.4, 1],
         storeOp: "store",
     }]
 })
+
+pass.setPipeline(cellPipeline)
+pass.setVertexBuffer(0, vertexBuffer)
+pass.draw(vertices.length / 2)
+
 pass.end()
 
-const commandBuffer = encoder.finish()
-
-device.queue.submit([commandBuffer])
 device.queue.submit([encoder.finish()])
