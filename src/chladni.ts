1aea6373595ecf251d139fff76c8e25494749538
Gnlow <gnlow@protonmail.com>
2024-04-27 20:11:25 +0900

    chladni animate

--- src/chladni.ts
+++ src/chladni.ts
@@ -91,6 +91,24 @@ const render = () => {
     if (!(frameCount % 100)) console.log(frameCount)
 }
 
+const observer = new ResizeObserver(entries => {
+    entries.forEach(entry => {
+        const canvas = entry.target as HTMLCanvasElement
+        const width = entry.contentBoxSize[0].inlineSize
+        const height = entry.contentBoxSize[0].blockSize
+        canvas.width = Math.max(1, Math.min(
+            width,
+            device.limits.maxTextureDimension2D!,
+        ))
+        canvas.height = Math.max(1, Math.min(
+            height,
+            device.limits.maxTextureDimension2D!,
+        ))
+        console.log(width, height)
+    })
+})
+observer.observe($canvas)
+
 while (true) {
     await waitTick()
     render()
